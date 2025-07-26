3bfe351bd269e33e064574e63b4c5f597cfa34df
Omar Hassan <omarhassanhhh7@gmail.com>
2025-07-26 12:20:33 +0300

    delete file attchamnet

--- snippet_website/models/ir_attachment.py
+++
@@ -1,8 +0,0 @@
-
-from odoo import models, fields
-
-# class IrAttachment(models.Model):
-#     _inherit = 'ir.attachment'
-
-#     active= fields.Boolean ('Active', help="If the active field is set to False, it will allow you to hide the top without removing it.",default=True)
-
